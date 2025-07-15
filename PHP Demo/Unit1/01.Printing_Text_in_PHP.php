e5acc1030cc864138606e17947284ea86f899776
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-07-15 22:47:36 +0530

    Add files via upload

---
+++ PHP Demo/Unit1/01.Printing_Text_in_PHP.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>
+            Printing the text in PHP
+        </title>
+    </head>
+    <body>
+        <?php echo "Hello, World<br>";?>
+        <?php echo "Aagam";?>
+    </body>
+</html>
