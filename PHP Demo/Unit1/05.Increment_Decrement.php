3ab5019ebb2cdb3e905a9c7a133158f0f9d2d427
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-07-19 19:50:51 +0530

    Add files via upload

---
+++ PHP Demo/Unit1/05.Increment_Decrement.php
@@ -0,0 +1,11 @@
+<?php
+    $a=10;
+    $b=10;
+
+    echo "Pre-increment :".(++$a)."<br>";
+    echo "Post-increment :".($a++)."<br>";
+    echo "After post-increment :".($a)."<br>";
+    echo "Pre-decrement :".(--$b)."<br>";
+    echo "Post-decrement :".($b--)."<br>";
+    echo "After post-decrement :".($b)."<br>";
+?>
