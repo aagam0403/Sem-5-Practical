a5d6feb25d8562adcab90aa9bb775d98a99e1973
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-07-22 14:18:09 +0530

    Add files via upload

---
+++ PHP Demo/Unit1/08.Comparison_Operator.php
@@ -0,0 +1,9 @@
+<!--Comparision Operator-->
+<?php
+    $a=10;
+    $b=20;
+    echo ($a==$b?"Equal":"Not Equal")."<br>";
+    echo ($a!=$b?"Not Equal":"Equal")."<br>";
+    echo ($a<$b?"A is smaller":"B is smaller")."<br>";
+    echo ($a>$b?"B is not greater":"A is not greater")."<br>";
+?>
