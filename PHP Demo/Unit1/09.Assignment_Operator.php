a94c373c299cd530fa666839dba1a6d2456f410c
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-07-24 14:21:57 +0530

    Add files via upload

---
+++ PHP Demo/Unit1/09.Assignment_Operator.php
@@ -0,0 +1,15 @@
+<?php
+    $x = 20;
+    $x += 5;
+
+    echo "After += : $x \n";
+
+    $x -= 3;
+    echo "After -= : $x \n";
+
+    $x *= 2;
+    echo "After *= : $x \n";
+
+    $x /= 3;
+    echo "After /= : $x \n";
+?>
