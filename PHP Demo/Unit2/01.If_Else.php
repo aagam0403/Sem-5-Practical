9a7eae2412cd07dd2aa4249b41e0b82bb9d9ed7f
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-07-29 13:43:16 +0530

    Add files via upload

---
+++ PHP Demo/Unit2/01.If_Else.php
@@ -0,0 +1,31 @@
+<?php
+    $too = 2;
+    if ($too == 2) 
+    {
+        echo "Number is 2\n";
+    }
+
+    $t = 20;
+    if ($t > 2) 
+    {
+        echo "Number is Greater than 2\n";
+    }
+    else 
+    {
+        echo "Number is less than or equal to 2\n";
+    }
+
+    $age = 20;
+    if ($age > 20) 
+    {
+        echo "Number is Greater than 20\n";
+    }
+    else if ($age > 5) 
+    {
+        echo "Number is Greater than 5\n";
+    }
+    else 
+    {
+        echo "Number is smaller\n";
+    }
+?>
