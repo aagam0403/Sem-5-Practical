6feb095a82708ce91460d2aa3443925fdf641716
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-08-04 15:37:36 +0530

    Add files via upload

---
+++ Python_Assignment/Unit1/01.one(1).py
@@ -0,0 +1,15 @@
+#Create a Python program that will have 3 variables which stores integer, float and
+#complex value. Display its value and also demonstrate its datatype class using type().
+
+int_var = 10
+float_var = 3.14
+complex_var = 2 + 5j
+
+print("Integer value:", int_var)
+print("Type of int_var:", type(int_var))
+
+print("\nFloat value:", float_var)
+print("Type of float_var:", type(float_var))
+
+print("\nComplex value:", complex_var)
+print("Type of complex_var:", type(complex_var))
