d2b7183484ad5ecf0a691568e5516e7e651c2447
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-08-05 15:31:38 +0530

    Add files via upload

---
+++ Python_Assignment/Unit1/02.py
@@ -0,0 +1,17 @@
+# Define the list
+festivals = ['Navratri', 'Diwali', 'Holi', 'Rakshabandhan']
+
+# 1. Print whole list
+print("Whole list:", festivals)
+
+# 2. Print only first element of list
+print("First element:", festivals[0])
+
+# 3. Print elements starting from 2nd till 3rd
+print("2nd to 3rd elements:", festivals[1:3])
+
+# 4. Print elements starting from 2nd element till last
+print("2nd element to last:", festivals[1:])
+
+# 5. Print whole list 4 times
+print("List repeated 4 times:", festivals * 4)
