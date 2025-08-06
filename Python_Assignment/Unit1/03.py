b1122ad3f4ae825ee164500eb307fbcddeefccba
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-08-06 14:18:46 +0530

    Add files via upload

---
+++ Python_Assignment/Unit1/03.py
@@ -0,0 +1,9 @@
+fruit_dict = {
+    "apple": "red",
+    "banana": "yellow",
+    "grape": "purple",
+    "orange": "orange"
+}
+
+print("Keys:", fruit_dict.keys())
+print("Values:", fruit_dict.values())
