84fe915859a207cfd416b5c08b9094aacd2933a9
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-08-06 14:19:02 +0530

    Add files via upload

---
+++ Python_Assignment/Unit1/04.py
@@ -0,0 +1,7 @@
+vegetables = ('Brinjal', 'Tomato', 'Cabbage', 'Cauliflower')
+
+print("Whole tuple:", vegetables)
+print("First element:", vegetables[0])
+print("2nd to 4th elements:", vegetables[1:4])
+print("2nd element to last:", vegetables[1:])
+print("Tuple repeated twice:", vegetables * 2)
