5f4b376e4963db7d59a97bf6ecb37dfed1de0cd1
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-08-07 12:46:43 +0530

    Add files via upload

---
+++ Python_Assignment/Unit1/07.py
@@ -0,0 +1,10 @@
+# Area of Rectangle
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+rectangle_area = length * breadth
+print("Area of the rectangle:", rectangle_area)
+
+# Area of Square
+side = float(input("Enter the side of the square: "))
+square_area = side * side
+print("Area of the square:", square_area)
