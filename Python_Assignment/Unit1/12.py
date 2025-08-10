7f7bffd8fe50bd64cc8de6d56a5a137cbf04ecc5
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-08-10 09:17:44 +0530

    Add files via upload

---
+++ Python_Assignment/Unit1/12.py
@@ -0,0 +1,4 @@
+import random
+print("Random integer between 1 and 10:", random.randint(1, 10))
+print("Random float between 0 and 1:", random.random())
+print("Random choice from list:", random.choice(['red', 'green', 'blue']))
