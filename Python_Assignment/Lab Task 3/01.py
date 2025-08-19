8192111b82135a23146713b84e1e6b3d0b014245
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-08-19 14:27:18 +0530

    Update 01.py

--- Python_Assignment/Lab Task 3/01.py
+++ Python_Assignment/Lab Task 3/01.py
@@ -3,7 +3,6 @@ def factorial(n):
     for i in range(1, n + 1):
         result *= i
     return result
-
-# Test cases
+    
 print("Factorial of 5 =", factorial(5))
 print("Factorial of 7 =", factorial(7))
