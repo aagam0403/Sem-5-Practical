aa9871369e4827453e4e5e1b3430b7437a337c0a
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-09-07 11:16:04 +0530

    Add files via upload

---
+++ PHP_Assignments/Lab_Exercise/Lab_Task_4/lab-4_10_1.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Form</title>
+</head>
+<body>
+	<form action="lab-4_10_2.php" method="POST">
+		<table border="1">
+			<tr>
+				<td>Number 1</td>
+				<td><input type="number" name="n1"></td>
+			</tr>
+			<tr>
+				<td>Number 2</td>
+				<td><input type="number" name="n2"></td>
+			</tr>
+			<tr>
+				<td colspan="2"><input type="submit"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
