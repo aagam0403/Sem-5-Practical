4a046813bf6dbda7bfb8ba21869483e5b460f030
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-10-04 17:06:13 +0530

    Update 7.py

--- Python_Assignment/Assignment 3/7.py
+++ Python_Assignment/Assignment 3/7.py
@@ -2,5 +2,7 @@ def employee(name, salary=10000):
     print("Employee Name:", name)
     print("Employee Salary:", salary)
 
-employee("Rahul", 25000)
-employee("Neha")   # default salary
+employee("Aagam", 25000)
+employee("Saiyam")   # default salary
+employee("Tarak", 30000)
+employee("Hetvi")    # default salary
